3b6e962690de1387b019b7e50ba80d711d6b206c
chandra2407 <chandra240797@gmail.com>
2025-01-09 02:11:15 +0530

    first

---
+++ index.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let score = 0;
+    const mole = document.querySelector('.mole');
+    const grounds = document.querySelectorAll('.ground');
+    let currentGround = Math.floor(Math.random() * grounds.length);
+    let spawnInterval = 1200; // Start with 1.2 seconds
+    function getRandomGround() {
+      let randomGround = Math.floor(Math.random() * grounds.length);
+      if(randomGround === currentGround) {
+        currentGround = randomGround === 0 ? randomGround + 1 : randomGround - 1;
+        return currentGround
+      }
+      currentGround = randomGround;
+      return currentGround;
+    }
+    function spawnMole() {
+      currentGround = getRandomGround();
+      let rect = grounds[currentGround].getBoundingClientRect();
+        mole.style.top = `${rect.top + -50}px`;
+        mole.style.left = `${rect.left + 30}px`;
+        let randomImage = Math.floor(Math.random() * 10);
+        mole.setAttribute('src', `flowers/${randomImage}.png`);
+        mole.style.display = 'block';
+        mole.style.animationDuration = `${spawnInterval / 1000}s`;
+    }
+    mole.addEventListener('click', () => {
+      score++;
+      document.querySelector('.score').textContent = score;
+      mole.style.display = 'none';
+      if (spawnInterval > 500) { // Decrease interval until it reaches 0.5 seconds
+        spawnInterval -= 20;
+        clearInterval(spawnIntervalId);
+        spawnIntervalId = setInterval(spawnMole, spawnInterval);
+      }
+    });
+    let spawnIntervalId = setInterval(spawnMole, spawnInterval);
+});
